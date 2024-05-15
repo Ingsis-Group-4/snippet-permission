1e60dc0eb8b8ea06f18eda63643c769c818243d2
Matías Bregoli <matias.bregoli@ing.austral.edu.ar>
2024-05-15 07:00:04 -0300

    Added exception handler

---
+++ src/main/kotlin/app/permission/ExceptionHandler.kt
@@ -0,0 +1,16 @@
+package app.permission
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class ValidationExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationExceptions(ex: MethodArgumentNotValidException): ResponseEntity<Any> {
+        val errors = ex.bindingResult.fieldErrors.map { it.defaultMessage }
+        val errorResponse = mapOf("message" to "Validation failed", "errors" to errors)
+        return ResponseEntity.badRequest().body(errorResponse)
+    }
+}
--- src/main/kotlin/app/permission/controller/PermissionController.kt
+++ src/main/kotlin/app/permission/controller/PermissionController.kt
@@ -9,8 +9,6 @@ import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.MethodArgumentNotValidException
-import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -47,12 +45,5 @@ class PermissionController(
         @PathVariable userId: String,
     ): ResponseEntity<List<SnippetOutput>> {
         return ResponseEntity.ok(permissionService.getAllSnippets(userId, permissionTypeInput))
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleValidationExceptions(ex: MethodArgumentNotValidException): ResponseEntity<Any> {
-        val errors = ex.bindingResult.fieldErrors.map { it.defaultMessage }
-        val errorResponse = mapOf("message" to "Validation failed", "errors" to errors)
-        return ResponseEntity.badRequest().body(errorResponse)
     }
 }
