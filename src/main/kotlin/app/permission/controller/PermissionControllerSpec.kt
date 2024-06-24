92e68b04e4cc67b0921c75ac5fffcd83a36401df
Matías Bregoli <matias.bregoli@ing.austral.edu.ar>
2024-06-24 17:41:24 -0300

    Added pagination for get all endpoint

--- src/main/kotlin/app/permission/controller/PermissionControllerSpec.kt
+++ src/main/kotlin/app/permission/controller/PermissionControllerSpec.kt
@@ -1,18 +1,21 @@
 package app.permission.controller
 
 import app.permission.model.dto.CreatePermissionInput
-import app.permission.model.dto.PermissionOutput
+import app.permission.model.dto.PermissionListOutput
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.parameters.RequestBody
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @RequestMapping("permission")
 interface PermissionControllerSpec {
@@ -36,7 +39,7 @@ interface PermissionControllerSpec {
     )
     fun createSnippet(input: CreatePermissionInput): ResponseEntity<Unit>
 
-    @GetMapping("all/{userId}")
+    @GetMapping("all")
     @Operation(
         summary = "Get all permissions from a user",
         responses = [
@@ -46,8 +49,10 @@ interface PermissionControllerSpec {
         ],
     )
     fun getAllUserPermissions(
-        @PathVariable("userId") userId: String,
-    ): List<PermissionOutput>
+        @AuthenticationPrincipal jwt: Jwt,
+        @RequestParam("page_num") pageNum: Int,
+        @RequestParam("page_size") pageSize: Int,
+    ): PermissionListOutput
 
     @DeleteMapping("all/{snippetId}")
     fun deleteAllPermissionsForSnippet(
